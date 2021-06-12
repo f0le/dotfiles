f4cd38d3e9c4d44285e40d8cd4618af04ff19e7b
f0le <info@folie.dev>
2021-06-12 12:43:39 +0200

    Delete script folder delete script folder because script folder is tracked seperately

--- .bin/monitor-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-intern=eDP-1
-extern=HDMI-2
-
-if xrandr --listactivemonitors | grep "$extern"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-else
-    xrandr --output "$intern" --auto --output "$extern" --auto
-fi
